80dca412e52cae51d4c808420f19b75681a406a4
kdpsa <carloseduardo1996@outlook.com>
2019-08-13 11:40:19 -0300

    Exercício de JS

---
+++ js/calc.js
@@ -0,0 +1,27 @@
+function operacao(op) {
+    var txta = document.querySelector('input#txta')
+    var txtb = document.querySelector('input#txtb')
+    var res = document.querySelector('div#res')
+    var n1 = Number(txta.value)
+    var n2 = Number(txtb.value)
+    var s;
+    switch (op) {
+        case 'soma':
+            s = (n1 + n2)
+            break
+        case 'sub':
+            s = (n1 - n2)
+            break;
+        case 'mult':
+            s = (n1 * n2)
+            break;
+        case 'div':
+            if (n2 == 0) {
+                s = 'Número não divisível por <strong>0</strong>.'
+            } else {
+                s = (n1 / n2)
+            }
+            break;
+    }
+    res.innerHTML = `${s}`
+}
